0ea0461f54221d32515ad7bf7e941d4d7564605b
whc <284075763@qq.com>
2018-08-30 17:53:10 +0800

    m

--- app/src/main/java/com/shuiyu/zhuan/utils/RetrofitFactory.java
+++ app/src/main/java/com/shuiyu/zhuan/utils/RetrofitFactory.java
@@ -7,7 +7,6 @@ import com.shuiyu.zhuan.api.BaseApi;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -33,9 +32,9 @@ public class RetrofitFactory {
                             .writeTimeout(DEFAULT_TIME_OUT, TimeUnit.SECONDS)
                             .retryOnConnectionFailure(true);
                     // Log 拦截器
-//                     HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-//                     interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-//                     builder.addInterceptor(interceptor);
+                     /*HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+                     interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+                     builder.addInterceptor(interceptor);*/
 
                     retrofit = new Retrofit.Builder()
                             .baseUrl(BaseApi.HOST)
